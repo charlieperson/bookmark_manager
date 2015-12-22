e70e6f53853f977470259a06476d0bbb8a54f533
Charlie Person <charlie.person@hyperisland.se>
2015-12-22 16:21:25 +0000

    CH20 completed

--- app/app.rb
+++ app/app.rb
@@ -1,29 +1,37 @@
 ENV['RACK_ENV'] ||= 'development'
 
+require 'sinatra/flash'
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class Bookmark < Sinatra::Base
+  register Sinatra::Flash
+
   helpers do
     def current_user
      @current_user ||= User.get(session[:user_id])
     end
   end
 
-
     enable :sessions
     set :session_secret, 'super secret'
 
   get '/' do
+    @user = User.new
     erb(:index)
   end
 
   post '/' do
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
                        password: params[:password],
                        password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/links')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb(:index)
+    end
   end
 
   get '/links' do
@@ -35,13 +43,6 @@ class Bookmark < Sinatra::Base
     erb(:add)
   end
 
-  get '/tags/:name' do
-    tag = Tag.first(name: params[:name])
-    @links = tag ? tag.links : []
-    erb(:links)
-  end
-
-
   post '/add' do
     link = Link.create(url: params[:url], title: params[:title])
     params[:tags].split(' ').each do |tag|
@@ -50,6 +51,13 @@ class Bookmark < Sinatra::Base
     link.save
     redirect '/links'
   end
+
+  get '/tags/:name' do
+    tag = Tag.all(name: params[:name])
+    @links = tag ? tag.links : []
+    erb(:links)
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
